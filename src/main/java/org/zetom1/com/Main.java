41bfd5993ec48718c09d860bb697c35f411f33d0
Zetom1 <43691719+Zetom1@users.noreply.github.com>
2024-03-07 22:16:29 -0600

    Deleting useless files

--- src/main/java/org/zetom1/com/Main.java
+++ src/main/java/org/zetom1/com/Main.java
@@ -1,7 +1,5 @@
 package org.zetom1.com;
 
-import org.zetom1.com.facade.ExcelWatermarkPoiFacadeImpl;
-
 public class Main {
     public static void main(String []args){
         System.out.println("This program generate an Excel document with a printable and in document watermark");
