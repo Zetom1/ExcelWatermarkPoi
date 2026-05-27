c4f6f80e72a4fe9f22711fdc2f72d5f5d8675cc1
Zetom1 <43691719+Zetom1@users.noreply.github.com>
2026-05-27 13:42:07 -0600

    Corrige lógica en Main.java

--- src/main/java/org/zetom1/com/Main.java
+++ src/main/java/org/zetom1/com/Main.java
@@ -1,11 +1,13 @@
 package org.zetom1.com;
 
+import org.zetom1.com.service.ExcelWatermarkPoiSrvImpl;
+
 public class Main {
     public static void main(String []args){
         System.out.println("This program generate an Excel document with a printable and in document watermark");
         System.out.println("");
         try{
-            ExcelWatermarkPoiFacadeImpl.generateDocument();
+            ExcelWatermarkPoiSrvImpl.generateDocument();
         }
         catch (Exception e){
             e.printStackTrace();
